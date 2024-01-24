ec17b87ed19b7d5f99777bed25a4c2458dd8b722
jstcode <jstorres0211@gmail.com>
2024-01-23 19:39:34 -0500

    feat: basic ui components

---
+++ src/components/ui/Logo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { darkTheme } from "@store/store"
+    import light from "$lib/assets/logos/light.png";
+    import dark from "$lib/assets/logos/dark.png";
+
+    let isDark: boolean = false;
+
+    darkTheme.subscribe((value) => isDark = value.darkTheme)
+</script>
+
+<img class="h-16 w-16 p-2" alt="The Krowen logo" src={isDark ? light : dark} />
