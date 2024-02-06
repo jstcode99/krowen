ca50eacd17dd05b657a80b53525239313985b36e
jstcode <jstorres0211@gmail.com>
2024-02-05 20:52:42 -0500

    feat: unsed pages and ui component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,16 @@
-<script lang="ts">
-  import { darkTheme } from "../store/store"
-  import { onMount } from 'svelte';
+<script>
+  import Hero from "@components/home/Hero.svelte";
+  import WhatWeDo from "@components/home/WhatWeDo.svelte";
+  import OurApps from "@components/home/OurApps.svelte";
+  import RecentlyWorks from "@components/home/RecentlyWorks.svelte";
 
-  import Benefits from "../components/home/Benefits.svelte";
-  import CardHero from "../components/home/CardHero.svelte";
-  import GetInTouch from "../components/home/GetInTouch.svelte";
-  import Hero from "../components/home/Hero.svelte";
-  import OurApps from "../components/home/OurApps.svelte";
-  import WhatWeDo from "../components/home/WhatWeDo.svelte";
+  import GetInTouch from "@components/home/GetInTouch.svelte";
+  import CardHero from "@components/home/CardHero.svelte";
 
-  let isDark: boolean = false;
-
-  darkTheme.subscribe((value) => isDark = value.darkTheme)
-
-  onMount(() => {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches || isDark) {
-          document.documentElement.classList.add('dark');
-          darkTheme.set({ darkTheme: true});
-        } else {
-          document.documentElement.classList.remove('dark');
-          darkTheme.set({ darkTheme: false});
-        }
-  });
 </script>
 <Hero/>
 <WhatWeDo/>
-<Benefits />
 <OurApps />
+<RecentlyWorks />
 <GetInTouch />
 <CardHero />
--- src/routes/about-us/+page.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts">
-
-</script>
-<h1>about-us</h1>
