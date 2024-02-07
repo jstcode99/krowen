569b226f0f516bfe64450f6b92622065e0896592
jstcode <jstorres0211@gmail.com>
2024-02-07 15:40:22 -0500

    feat: improve and resize and style list and add final dexcriptions

--- src/components/services/components/CardWebSite.svelte
+++ src/components/services/components/CardWebSite.svelte
@@ -11,29 +11,31 @@
   }
   let webSite: IWebSite | null = null;
   let rotatation = 0;
-  
+
   export { webSite, rotatation };
 </script>
 
 <article
   id={"card-website" + webSite?.id}
-  class={`w-[280px] h-[580px] px-6 py-8 origin-top-left
+  class={`w-[280px] px-6 py-8 origin-top-left
     bg-gradient-to-b from-gray-900 to-black
     rounded-md shadow border
-  border-slate-400 flex-col justify-center content-start
+  border-slate-400 flex-col content-start
     gap-2 inline-flex cursor-pointer rotate-[${rotatation}deg] 
-    hover:scale-105 transform-gpu hover:border-k-green`}
+    hover:scale-105 duration-300 hover:shadow-sm transform-gpu hover:border-k-green`}
 >
-  <div class="flex flex-col justify-start items-start gap-y-4 p-2">
+  <div class="flex flex-col justify-start gap-y-4 p-2">
     <h1 class="text-white text-md font-bold capitalize">
       Web site <span class="text-k-green">{webSite?.name}</span>
     </h1>
-    <div class="w-md text-white text-sm font-normal">
+    <div class="w-md text-white text-sm font-normal text-left">
       {webSite?.summary}
     </div>
   </div>
-  <div class="w-full flex flex-col justify-start items-start gap-2">
-    <span class="w-full text-white text-3xl font-extrabold text-center">${webSite?.price}</span>
+  <div class="w-full flex flex-col justify-start gap-2">
+    <span class="w-full text-white text-3xl font-extrabold text-center"
+      >${webSite?.price}</span
+    >
     <div class="w-full flex justify-center items-center">
       <button
         type="button"
@@ -45,7 +47,7 @@
         Comprar ahora!
       </button>
     </div>
-    <h3 class="text-white text-sm font-semibold">Great for</h3>
+    <h3 class="text-white text-sm font-semibold">Hechas para</h3>
     <p class="text-left text-gray-400 text-xs font-normal">
       {webSite?.subtext}
     </p>
@@ -53,16 +55,16 @@
   <div class="flex-col justify-start items-start gap-4 flex">
     <h4 class="text-white text- font-semibold">Basic web features</h4>
     <div class="justify-center items-center gap-2 inline-flex">
-      <ul
-        class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400"
-      >
+      <nav class="flex flex-col sm:items-start sm:text-left text-center text-white text-sm items-center -mb-1 space-y-2.5">
         {#each webSite?.features ?? [] as feature}
-          <li class="flex items-center gap-x-2 text-sm">
-            <CircleCheckIcon />
-            {feature}
-          </li>
+            <a>
+              <span class="text-k-green w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                <CircleCheckIcon />
+              </span>
+              {feature}
+            </a>
         {/each}
-      </ul>
+      </nav>
     </div>
   </div>
 </article>
