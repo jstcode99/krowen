192034c905536422f32e5b21614a3a57247a6eb0
jstcode <jstorres0211@gmail.com>
2024-02-07 15:39:45 -0500

    feat: responsive hero

--- src/components/home/sections/Hero.svelte
+++ src/components/home/sections/Hero.svelte
@@ -1,111 +1,46 @@
-<div
-  class="relative mx-auto mt-16 grid w-full max-w-6xl grid-cols-1 px-4 sm:mt-20 sm:px-6 lg:px-8 xl:mt-32"
->
-  <div
-    class="col-start-1 row-start-1 h-7 text-base font-semibold leading-7 text-k-green text-left"
-    aria-hidden="true"
-  >
-    Acompañamiento desde la idea hasta la implementación
-  </div>
-  <h1
-    class="col-start-1 row-start-2 mt-4 max-w-[36rem] text-4xl 
-    font-extrabold tracking-tight sm:text-7xl xl:max-w-[43.5rem]
-    text-left"
-  >
-  Construya su próxima idea con nostros.
-  </h1>
-  <p class="col-start-1 row-start-3 mt-4 max-w-lg text-lg text-left">
-    Creamos productos digitales que resuelven problemas y generan impacto.
-    usando tecnologías modernas y metodologías ágiles.
-    apostamos por la calidad y la innovación en las soluciones que ofrecemos.
-  </p>
-  <div
-    class="col-start-1 row-start-4 mt-10 flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0"
-  >
-    <a
-      class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-black text-white hover:bg-black"
-      href="/components"
-      ><span
-        >Estimar proyecto <span
-          aria-hidden="true"
-          class="hidden text-black sm:inline">→</span
-        ></span
-      ></a
-    ><a
-      class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-white/0 dark:bg-white 
-      text-black ring-1 ring-black/10 hover:bg-white/25 hover:ring-black/15"
-      href="/#contact"
-      ><span
-        >Contactar ahora <span
-          aria-hidden="true"
-          class="hidden text-black/25 sm:inline">→</span
-        ></span
-      ></a
-    >
-  </div>
-  <div
-    class="pointer-events-none col-start-1 row-start-5 flex md:row-span-3 md:row-start-3 lg:row-span-4 lg:row-start-2 xl:row-span-5 xl:row-start-1 xl:justify-end"
-  >
-    <div
-      class="-ml-[32rem] mt-12 h-[46.375rem] origin-top scale-[calc(204/299)] select-none sm:-ml-[24rem] sm:-mt-20 sm:h-auto sm:transform-none md:-ml-64 md:mt-10 lg:-ml-16 lg:mt-0 xl:-mr-4 xl:ml-0"
-    >
-      <div class="flex justify-end">
-        <div class="relative flex items-end">
-          <div
-            class="absolute -inset-x-8 bottom-0 h-px bg-black/15 [mask-image:linear-gradient(to_right,transparent,white_4rem,white_calc(100%-4rem),transparent)]"
-          ></div>
-          <div class="absolute left-16 top-full -mt-px h-8 overflow-hidden">
-            <div class="flex -mt-px h-[2px] w-56">
-              <div
-                class="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"
-              ></div>
-              <div
-                class="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"
-              ></div>
-            </div>
-          </div>
-          <div class="relative z-10 p-4">
-            <div
-              class="absolute -inset-y-8 right-0 w-px bg-black/15 [mask-image:linear-gradient(to_top,transparent,white_4rem,white_calc(100%-4rem),transparent)]"
-            ></div>
-            <div
-              class="absolute -inset-y-8 left-0 w-px bg-black/15 [mask-image:linear-gradient(to_top,transparent,white_4rem,white_calc(100%-4rem),transparent)]"
-            ></div>
-            <div
-              class="absolute -inset-x-8 top-0 h-px bg-black/15 [mask-image:linear-gradient(to_left,transparent,white_4rem,white_calc(100%-4rem),transparent)]"
-            ></div>
-            <div
-              class="absolute bottom-full left-40 -mb-px flex h-8 items-end overflow-hidden"
-            >
-              <div class="flex -mb-px h-[2px] w-56">
-                <div
-                  class="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"
-                ></div>
-                <div
-                  class="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"
-                ></div>
-              </div>
-            </div>
-            <div
-              class="w-[24.5rem] divide-y divide-black/20 rounded-lg bg-white text-[0.8125rem] leading-5 text-black shadow-xl shadow-black/5 ring-1 ring-black/10"
-            >
-              <div class="p-4">
-                <div class="text-center font-medium">Mockup here</div>
-                <div class="mt-4 space">
+<script>
+  import banner from "$lib/assets/sections/hero/banner.png";
+</script>
 
-                </div>
-              </div>
-              <div class="p-4">
-                <div
-                  class="pointer-events-auto rounded-md px-4 py-2 text-center font-medium shadow-sm ring-1 ring-black/10 hover:bg-black"
-                >
-                  View all
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+<section class="relative mt-12">
+  <div class="container px-5 py-4 mx-auto flex sm:flex-nowrap flex-wrap">
+    <div
+      class="lg:w-1/2 md:1/2 sm:w-full flex flex-col justify-center px-12 md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+    >
+      <div
+        class="col-start-1 row-start-1 h-7  mb-3 text-base font-semibold leading-7 text-k-green text-left"
+        aria-hidden="true"
+      >
+        Acompañamiento desde la idea hasta la implementación
+      </div>
+      <h1
+        class="col-start-1 row-start-2 mt-4 max-w-[36rem] text-4xl font-extrabold tracking-tight sm:text-7xl xl:max-w-[43.5rem] text-left"
+      >
+        Construya su próxima idea con nostros.
+      </h1>
+      <p class="col-start-1 row-start-3 mt-4 max-w-lg text-lg text-left">
+        Creamos productos digitales que resuelven problemas y generan impacto.
+        usando tecnologías modernas y metodologías ágiles. apostamos por la
+        calidad y la innovación en las soluciones que ofrecemos.
+      </p>
+      <div
+        class="col-start-1 row-start-4 mt-10 flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0"
+      >
+        <button
+          type="button"
+          class="text-black dark:text-white
+            focus:outline-none focus:ring-4 font-medium
+            rounded-lg text-sm px-5 py-2.5 me-2 mb-2
+          border-black dark:border-white border-2 hover:scale-105"
+        >
+          Estimar Proyecto
+        </button>
       </div>
     </div>
+    <img
+      class="hidden lg:flex lg:w-2/3 md:w-1/2 sm:mr-10 p-10  object-cover"
+      src={banner}
+      alt="Hero mockup"
+    />
   </div>
-</div>
+</section>
