155056b9c2f6f046db14e56ae350ec00c68e8cca
jstcode <jstorres0211@gmail.com>
2024-02-10 12:58:55 -0500

    fix: responsive

--- src/components/home/sections/Hero.svelte
+++ src/components/home/sections/Hero.svelte
@@ -1,46 +1,55 @@
 <script>
-  import banner from "$lib/assets/sections/hero/banner.png";
+  import hero from "$lib/assets/sections/hero/hero-home.png";
 </script>
 
-<section class="relative mt-12">
-  <div class="container px-5 py-4 mx-auto flex sm:flex-nowrap flex-wrap">
+<section class="relative xl:mt-16">
+  <div class="max-w-6xl xl:h-96 sm:h-screen px-5 py-4 mx-auto flex sm:flex-nowrap flex-wrap mb-16">
     <div
-      class="lg:w-1/2 md:1/2 sm:w-full flex flex-col justify-center px-12 md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+      class="lg:absolute md:static mt-16 z-10 w-container flex flex-col justify-center"
     >
       <div
-        class="col-start-1 row-start-1 h-7  mb-3 text-base font-semibold leading-7 text-k-green text-left"
-        aria-hidden="true"
+        class="mb-3 text-xl font-semibold leading-7 text-k-purple text-left"
+      
       >
         Acompañamiento desde la idea hasta la implementación
       </div>
       <h1
-        class="col-start-1 row-start-2 mt-4 max-w-[36rem] text-4xl font-extrabold tracking-tight sm:text-7xl xl:max-w-[43.5rem] text-left"
+        class="col-start-1 row-start-2 mt-4 text-4xl font-extrabold tracking-tight sm:text-7xl text-left"
       >
-        Construya su próxima idea con nostros.
+        Desarrolla tus <span class="text-k-purple">ideas</span> <br>con nosotros.
       </h1>
       <p class="col-start-1 row-start-3 mt-4 max-w-lg text-lg text-left">
         Creamos productos digitales que resuelven problemas y generan impacto.
-        usando tecnologías modernas y metodologías ágiles. apostamos por la
-        calidad y la innovación en las soluciones que ofrecemos.
+        usando tecnologías modernas y metodologías ágiles. apostamos por la <br>
+        calidad y la innovación br en las soluciones que ofrecemos.
       </p>
       <div
         class="col-start-1 row-start-4 mt-10 flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0"
       >
-        <button
-          type="button"
-          class="text-black dark:text-white
-            focus:outline-none focus:ring-4 font-medium
-            rounded-lg text-sm px-5 py-2.5 me-2 mb-2
-          border-black dark:border-white border-2 hover:scale-105"
+        <a
+          href="#browse"
+          class="inline-flex justify-center items-center rounded-lg text-sm font-semibold py-3 px-4 border border-k-purple text-k-purple hover:scale-105"
+          ><span
+            >Contactar <span class="hidden sm:inline">ahora</span>
+            <span>→</span></span
+          ></a
         >
-          Estimar Proyecto
-        </button>
+        <a
+          href="#browse"
+          class="inline-flex justify-center items-center rounded-lg text-sm font-semibold py-3 px-4 hover:scale-105"
+          ><span
+            >Contactar <span class="hidden sm:inline">ahora</span>
+            <span aria-hidden="true">→</span></span
+          ></a
+        >
       </div>
     </div>
-    <img
-      class="hidden lg:flex lg:w-2/3 md:w-1/2 sm:mr-10 p-10  object-cover"
-      src={banner}
-      alt="Hero mockup"
-    />
+    <div class="hidden absolute lg:block xl:block right-[10%] md:right-[-0%] xl:top-0 md:top-[20%] opacity-90 ">
+      <img
+        class="object-cover h-full xl:w-[1100px] lg:w-[600px]"
+        src={hero}
+        alt="Desarrollo tus ideas con nosotros"
+      />
+    </div>
   </div>
 </section>
