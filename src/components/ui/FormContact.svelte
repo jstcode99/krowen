b28b063b0177f60a3dc693ea4765f6704425a58c
jstcode <jstorres0211@gmail.com>
2024-02-16 20:32:21 -0500

    feat: mail form

--- src/components/ui/FormContact.svelte
+++ src/components/ui/FormContact.svelte
@@ -1,15 +1,52 @@
-<script>
+<script lang="ts">
   import Saos from "saos";
   import Input from "@ui/Input.svelte";
+  import emailjs from "@emailjs/browser";
+  import Toast from "@ui/Toast.svelte";
+  import { toast } from "@store/store";
+
+  const form = {
+    email: "",
+    name: "",
+    subject: "",
+    message: "",
+  };
+
+  const resetForm = () => {
+    form.email = "";
+    form.name = "";
+    form.subject = "";
+    form.message = "";
+  };
+
+  const handleSubmit = (e: SubmitEvent) => {
+    emailjs
+      .sendForm("service_hrofcks", "template_5pcv00p", e.target, {
+        publicKey: "xUnJaHMgInns7GS1X",
+      })
+      .then(
+        () => {
+          toast.success(
+            "<b>Genial!</b> Se ha enviado el mensaje a nuestra bandeja."
+          );
+        },
+        (error) => {
+          toast.error("<b>Ips!</b>, Intentalo mas tarde.");
+        }
+      );
+
+    resetForm();
+  };
 </script>
 
-<form class="w-full flex flex-col">
+<form class="w-full flex flex-col" on:submit|preventDefault={handleSubmit}>
+  <Toast />
   <Saos
     animation={"slide-in-right 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both"}
   >
     <div class="mb-6">
       <Input
-        value={""}
+        value={form.email}
         type="text"
         name="email"
         placeholder="tu-email@email.com"
@@ -17,11 +54,17 @@
       />
     </div>
     <div class="mb-6">
-      <Input value={""} type="text" name="name" placeholder="Nombre" required />
+      <Input
+        value={form.name}
+        type="text"
+        name="name"
+        placeholder="Nombre"
+        required
+      />
     </div>
     <div class="mb-6">
       <Input
-        value={""}
+        value={form.subject}
         type="text"
         name="subject"
         placeholder="Asunto"
@@ -30,7 +73,7 @@
     </div>
     <div class="mb-6">
       <Input
-        value={""}
+        value={form.message}
         type="textarea"
         name="message"
         placeholder="Mensaje"
@@ -39,7 +82,7 @@
     </div>
     <div class="flex items-start mb-6">
       <button
-        type="button"
+        type="submit"
         class="w-full text-k-gray-light dark:text-white
           focus:outline-none focus:ring-4 font-medium
           rounded-lg text-sm px-5 py-2.5 me-2 mb-2
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,18 @@
+import type { error } from '@sveltejs/kit';
 import { writable } from 'svelte/store';
 
 export interface Theme {
    darkTheme: boolean;
 }
 
-const initStore = () => {
+export interface Toast {
+   type: 'success' | 'error' | 'warning' | 'info';
+   message: string;
+   show?: boolean;
+}
+const initStoreTheme = () => {
    const initialTheme: Theme = {
-        darkTheme: false,
+      darkTheme: false,
    };
 
    const { subscribe, set, update } = writable(initialTheme);
@@ -14,13 +20,41 @@ const initStore = () => {
    return {
       subscribe,
       set: (theme: Theme) => set(theme),
-      toggle: () => 
-        update(({ darkTheme, ...rest }) => ({
-            ...rest,
-            darkTheme: !darkTheme
-        })),
+      toggle: () => update(({ darkTheme, ...rest }) => ({
+         ...rest,
+         darkTheme: !darkTheme
+      })),
       reset: () => set(initialTheme)
    };
 };
 
-export const darkTheme = initStore();
+const initToast = () => {
+   const initialToast: Toast = {
+      type: 'success',
+      message: '!Hola',
+      show: false
+   };
+
+   const { subscribe, set, update } = writable(initialToast);
+
+   return {
+      subscribe,
+      set: (toast: Toast) => set(toast),
+      success: (newMessage: string) => update(({ message, ...rest }) => ({
+         ...rest,
+         type: 'success',
+         message: newMessage,
+         show: true
+      })),
+      error: (newMessage: string) => update(({ message, ...rest }) => ({
+         ...rest,
+         type: 'error',
+         message: newMessage,
+         show: true
+      })),
+      reset: () => set(initialToast)
+   };
+};
+
+export const darkTheme = initStoreTheme();
+export const toast = initToast()
